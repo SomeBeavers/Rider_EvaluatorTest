8a12fb7a0de7700830eeb6e9c09f61f70575a213
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-09-07 11:45:25 +0200

    Update.

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -10,7 +10,15 @@ namespace Tests
         [Test]
         public void Test1()
         {
+            var list = new List<int>();
+            var method1 = list.Count == 0 ? Method1() : list.First();
+            Console.WriteLine(method1);
             Assert.Pass();
+        }
+
+        private int Method1()
+        {
+            return 1;
         }
     }
 }
